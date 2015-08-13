22861d2ab06a18b1ed1fce9ae50111ee887dc9a6
luosch <me@lsich.com>
2015-08-13 23:58:44 +0800

    setup Xcode project

---
+++ SCAvatarBrowser/SCAvatarBrowser/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SCAvatarBrowser
+//
+//  Created by 罗思成 on 15/7/30.
+//  Copyright (c) 2015年 罗思成. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
