2e43823789597be7afa7cba0eca80c02881f636b
luosch <me@lsich.com>
2015-09-02 23:39:15 +0800

    add SCAvatartBrowser

---
+++ Classes/SCAvatarBrowser.m
@@ -0,0 +1,13 @@
+//
+//  SCAvatarBrowser.m
+//  SCAvatarBrowserDemo
+//
+//  Created by 罗思成 on 15/9/2.
+//  Copyright (c) 2015年 罗思成. All rights reserved.
+//
+
+#import "SCAvatarBrowser.h"
+
+@implementation SCAvatarBrowser
+
+@end
